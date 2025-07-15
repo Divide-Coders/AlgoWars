638044d20265fc814f09f310d32c1e9ad20d5dad
matin0789 <matin.a0789@gmail.com>
2025-07-16 01:09:35 +0330

    .gitignore added for proj doc && city classes added

--- Src/map/city.py
+++ Src/map/city.py
@@ -1,4 +1,6 @@
 #Nima
+
+
 class City:
     def __init__(self, name, country, x, y, city_type, defense=0):
         self.name = name      
@@ -8,5 +10,15 @@ class City:
         self.city_type = city_type
         self.defense = defense
         self.has_py = False  #jassooss kasif
+
+# ! I code it tomarrow please dont touch them
+class EnemyCity(City):
+    pass
+
+class OwnCity(City):
+    pass
+
+class BaseCity(OwnCity):
+    pass
 
               
